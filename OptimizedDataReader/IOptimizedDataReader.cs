3a374fdc3aacd5ea3e0088c8841fe7049fe4f4ec
Alan Savage <alansavage.dev@gmail.com>
2017-06-10 16:19:16 +0100

    Initial commit

---
+++ OptimizedDataReader/IOptimizedDataReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Savage.Data
+{
+    public interface IOptimizedDataReader
+    {
+
+        bool Read();
+
+        bool NextResult();
+
+        bool GetBoolean(string columnName);
+
+        bool? GetNullableBoolean(string columnName);
+
+        string GetString(string columnName);
+
+        DateTime GetDateTime(string columnName);
+
+        DateTime? GetNullableDateTime(string columnName);
+
+        int GetInt32(string columnName);
+
+        int? GetNullableInt32(string columnName);
+
+        long GetInt64(string columnName);
+
+        long? GetNullableInt64(string columnName);
+
+        Guid GetGuid(string columnName);
+
+        Guid? GetNullableGuid(string columnName);
+
+        byte[] GetBytes(string columnName);
+    }
+}
