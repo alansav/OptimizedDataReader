e3d906669a6cef0e5e5e14bcec6bc17173e40720
Alan Savage <alansavage.dev@gmail.com>
2017-06-11 09:56:09 +0100

    Update fields in AssemblyInfo.cs

--- OptimizedDataReader/Properties/AssemblyInfo.cs
+++ OptimizedDataReader/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OptimizedDataReader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An optimized Data Reader which uses the ordinal index of a column for performance improvements while allowing the client code to still reference the column using the column name")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alan Savage")]
 [assembly: AssemblyProduct("OptimizedDataReader")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Alan Savage 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
